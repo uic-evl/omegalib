ad544bc3d55c903779c61df733f76b5b7f3d6b15
Alessandro Febretti <febret@gmail.com>
2013-11-30 22:59:05 -0600

    fixed camera output: now readback happens on endFrame

--- src/omega/CameraOutput.cpp
+++ src/omega/CameraOutput.cpp
@@ -1,29 +1,38 @@
-/**************************************************************************************************
+/******************************************************************************
  * THE OMEGA LIB PROJECT
- *-------------------------------------------------------------------------------------------------
- * Copyright 2010-2013		Electronic Visualization Laboratory, University of Illinois at Chicago
+ *-----------------------------------------------------------------------------
+ * Copyright 2010-2013		Electronic Visualization Laboratory, 
+ *							University of Illinois at Chicago
  * Authors:										
  *  Alessandro Febretti		febret@gmail.com
- *-------------------------------------------------------------------------------------------------
- * Copyright (c) 2010-2013, Electronic Visualization Laboratory, University of Illinois at Chicago
+ *-----------------------------------------------------------------------------
+ * Copyright (c) 2010-2013, Electronic Visualization Laboratory,  
+ * University of Illinois at Chicago
  * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions 
- * and the following disclaimer. Redistributions in binary form must reproduce the above copyright 
- * notice, this list of conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. 
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. Redistributions in binary 
+ * form must reproduce the above copyright notice, this list of conditions and 
+ * the following disclaimer in the documentation and/or other materials provided 
+ * with the distribution. 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF 
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *************************************************************************************************/
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *-----------------------------------------------------------------------------
+ * What's in this file
+ *	The Camera output class is used to simplify and optimize reading back 
+ *  rendered frames to main memory.
+ ******************************************************************************/
 #include "omega/CameraOutput.h"
 #include "omega/Camera.h"
 #include "omega/PixelData.h"
@@ -31,7 +40,7 @@
 
 using namespace omega;
 
-///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 CameraOutput::CameraOutput(): 
 	myEnabled(false), myRenderTarget(NULL), myType(RenderTarget::RenderOffscreen),
 	myReadbackColorTarget(NULL), myReadbackDepthTarget(NULL),
@@ -40,14 +49,14 @@ CameraOutput::CameraOutput():
 	reset(RenderTarget::RenderOffscreen);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::reset(RenderTarget::Type type)
 {
 	myType = type;
 	myRenderTarget = NULL;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::setReadbackTarget(PixelData* color, PixelData* depth)
 {
 	myReadbackColorTarget = color;
@@ -60,14 +69,14 @@ void CameraOutput::setReadbackTarget(PixelData* color, PixelData* depth)
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::setReadbackTarget(PixelData* color, PixelData* depth, const Rect& readbackViewport)
 {
 	setReadbackTarget(color, depth);
 	myReadbackViewport = readbackViewport;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::setTextureTarget(Texture* color, Texture* depth)
 {
 	myTextureColorTarget = color;
@@ -80,14 +89,14 @@ void CameraOutput::setTextureTarget(Texture* color, Texture* depth)
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::setTextureTarget(Texture* color, Texture* depth, const Rect& readbackViewport)
 {
 	setTextureTarget(color, depth);
 	myReadbackViewport = readbackViewport;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::beginDraw(const DrawContext& context)
 {
 	if(myRenderTarget == NULL)
@@ -105,36 +114,32 @@ void CameraOutput::beginDraw(const DrawContext& context)
 	myRenderTarget->bind();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::endDraw(const DrawContext& context)
 {
 	if(myRenderTarget != NULL)
 	{
-		// HACK
-		myRenderTarget->readback();
-
 		myRenderTarget->unbind();
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::startFrame(const FrameInfo& frame)
 {
 	if(myRenderTarget != NULL)
 	{
-		//lock();
 		myRenderTarget->clear();
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 void CameraOutput::finishFrame(const FrameInfo& frame)
 {
 	if(myRenderTarget != NULL)
 	{
-		//unlock();
-		// HACK
-		//myRenderTarget->readback();
+	    myRenderTarget->bind();
+		myRenderTarget->readback();
+    	myRenderTarget->unbind();
 	}
 }
 
