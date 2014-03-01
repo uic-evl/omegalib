1703af9aa29363b58c330a2e546c1cad84b37cf8
Alessandro Febretti <febret@gmail.com>
2014-03-01 03:09:51 -0600

    updated mcsend script to support multiple connect/disconnect cycles.

--- tools/mcsend.py
+++ tools/mcsend.py
@@ -1,11 +1,20 @@
+# This module implements a basic omegalib MissionControl client in python
+# usage example:
+# connect([host])       < connects to an application running on [host]
+# sendCommand('foo()')  < calls python function foo in the application
+# bye()                 < closes the connection
 import socket
 import struct
 
-s = socket.socket()
+s = None
 
 # Connect to a mission control server
 def connect(host = 'localhost', port = 22500):
+    global s
+    print('connecing')
+    s = socket.socket()
     s.connect((host, port))
+    print('connected')
 
 # Internal, send data buffer
 def send(msg, size):
@@ -25,6 +34,7 @@ def sendCommand(command):
 
 # Close the connection    
 def bye():
+    global s
     send("bye!", 4)
     l32bit = struct.pack('i', 0)
     send(l32bit, 4)
@@ -32,4 +42,5 @@ def bye():
         v = s.recv(20)
         if v == '':
             s.close()
+            s = None
             return
