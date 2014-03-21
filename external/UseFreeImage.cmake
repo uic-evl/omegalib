b3d822f9a23cfebe85ca023be537d45b63ea7104
Alessandro Febretti <febret@gmail.com>
2014-03-20 22:19:02 -0500

    CHANGE: FreeImage fixes for VS2013 FreeImage is now part of the repository (no need to download during build) The archive source code also contains small fixes needed to make FreeImage build in visual studio 2013

--- external/UseFreeImage.cmake
+++ external/UseFreeImage.cmake
@@ -1,10 +1,10 @@
 set(EXTLIB_NAME FreeImage)
-set(EXTLIB_TGZ ${CMAKE_BINARY_DIR}/${EXTLIB_NAME}.tar.gz)
+set(EXTLIB_TGZ ${CMAKE_SOURCE_DIR}/external/${EXTLIB_NAME}.tar.gz)
 set(EXTLIB_DIR ${CMAKE_BINARY_DIR}/${EXTLIB_NAME})
 
 if(NOT EXISTS ${EXTLIB_DIR})
-  message(STATUS "Downloading FreeImage source...")
-  file(DOWNLOAD http://omegalib.googlecode.com/files/${EXTLIB_NAME}.tar.gz ${EXTLIB_TGZ} SHOW_PROGRESS)
+  message(STATUS "Extracting FreeImage...")
+  #file(DOWNLOAD http://omegalib.googlecode.com/files/${EXTLIB_NAME}.tar.gz ${EXTLIB_TGZ} SHOW_PROGRESS)
   execute_process(COMMAND ${CMAKE_COMMAND} -E tar xzf
     ${EXTLIB_TGZ} WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
 endif(NOT EXISTS ${EXTLIB_DIR})
