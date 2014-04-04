4b8b9dd7061a7f52ba83be0065e42ea24866746f
Alessandro Febretti <febret@gmail.com>
2014-04-04 02:47:00 -0500

    CHANGE: python interpreter lock methods made public this is to allow interpreter locking from external modules that may want to run python commands from other threads.

--- include/omega/PythonInterpreter.h
+++ include/omega/PythonInterpreter.h
@@ -127,7 +127,10 @@ namespace omega
 		virtual void updateSharedData(SharedIStream& in);
 
 		//String getHelpString(const String& filter);
-	protected:
+        void lockInterpreter();
+        void unlockInterpreter();
+
+    protected:
 		struct QueuedCommand
 		{
 			QueuedCommand(const String& cmd, bool bneedsExecute, bool bneedsSend):
@@ -163,10 +166,6 @@ namespace omega
 		// Stats
 		Ref<Stat> myUpdateTimeStat;
 
-
-	private:
-		void lockInterpreter();
-		void unlockInterpreter();
 
 	private:
 		static const Event* mysLastEvent;
