89bea630e06d16ac3eefdc3fee434db1e2c88529
sufulow <sufulow@gmail.com>
2014-09-16 15:04:03 +0800

    add saveImage and use memcpy copy ffbmp to PixelData by line

--- src/omega/ImageUtils.cpp
+++ src/omega/ImageUtils.cpp
@@ -251,16 +251,7 @@ Ref<PixelData> ImageUtils::ffbmpToPixelData(FIBITMAP*& image, const String& file
     for(int i = 0; i < height; i++)
     {
         char* pixels = (char*)FreeImage_GetScanLine(image, i);
-        for(int j = 0; j < width; j++)
-        {
-            int k = i * width + j;
-            // TODO: optimize this with memcpy
-            data[k * pixelOffset + 0] = pixels[j * pixelOffset + 0];
-            data[k * pixelOffset + 1] = pixels[j * pixelOffset + 1];
-            data[k * pixelOffset + 2] = pixels[j * pixelOffset + 2];
-            if(bpp == 32) data[k * pixelOffset + 3] = pixels[j * pixelOffset + 3];
-            //data[j * pixelOffset + 3] = pixels[j * pixelOffset + 3];
-        }
+		memcpy( data + i * width * pixelOffset, pixels, width * pixelOffset );
     }
     pixelData->unmap();
     
@@ -346,6 +337,20 @@ Ref<PixelData> ImageUtils::loadImage(const String& filename, bool hasFullPath)
     FreeImage_Unload(image);
 
     return pixelData;
+}
+
+bool ImageUtils::saveImage(const String& filename, PixelData* data, ImageFormat format)
+{
+	FILE* fp = fopen(filename.c_str(), "wb");
+	if(!fp)
+	{
+		return false;
+	}
+
+	Ref<ByteArray> imageData = encode(data, format);
+	fwrite(imageData->getData(), imageData->getSize(), 1, fp);
+	fclose(fp);
+	return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
