9d49953b6e1f95d0e92094c8d83c11dd5697c263
Alessandro Febretti <febret@gmail.com>
2014-10-25 01:25:51 -0500

    untabify

--- include/omega/PythonInterpreter.h
+++ include/omega/PythonInterpreter.h
@@ -47,154 +47,154 @@ class PythonInteractiveThread;
 
 namespace omega
 {
-	///////////////////////////////////////////////////////////////////////////
-	//struct CommandHelpEntry
-	//{
-	//	String syntax;
-	//	String info;
-	//};
+    ///////////////////////////////////////////////////////////////////////////
+    //struct CommandHelpEntry
+    //{
+    //	String syntax;
+    //	String info;
+    //};
 
-	///////////////////////////////////////////////////////////////////////////
-	class OMEGA_API PythonInterpreter: public SharedObject
-	{
-		friend struct PythonInterpreterWrapper;
-	public:
-		enum CallbackType
-		{
-			CallbackUpdate, CallbackEvent, CallbackDraw
-		};
+    ///////////////////////////////////////////////////////////////////////////
+    class OMEGA_API PythonInterpreter: public SharedObject
+    {
+        friend struct PythonInterpreterWrapper;
+    public:
+        enum CallbackType
+        {
+            CallbackUpdate, CallbackEvent, CallbackDraw
+        };
 
-		//! The flags that can be applied to the runFile method
-		enum RunFlags
-		{
-			NoRunFlags = 0,
-			//! Set the current working directory to the script path
-			SetCwdToScriptPath = 1 << 1,
-			//! Adds the script path to the search path for python modules (loaded using the import command)
-			AddScriptPathToModuleSearchPath = 1 << 2,
-			DefaultRunFlags = SetCwdToScriptPath
-		};
+        //! The flags that can be applied to the runFile method
+        enum RunFlags
+        {
+            NoRunFlags = 0,
+            //! Set the current working directory to the script path
+            SetCwdToScriptPath = 1 << 1,
+            //! Adds the script path to the search path for python modules (loaded using the import command)
+            AddScriptPathToModuleSearchPath = 1 << 2,
+            DefaultRunFlags = SetCwdToScriptPath
+        };
 
-		//! @internal returns the last event received by the interpreter. Used for script interoperability
-		static const Event* getLastEvent() { return mysLastEvent; }
+        //! @internal returns the last event received by the interpreter. Used for script interoperability
+        static const Event* getLastEvent() { return mysLastEvent; }
 
-	public:
-		PythonInterpreter();
-		virtual ~PythonInterpreter();
+    public:
+        PythonInterpreter();
+        virtual ~PythonInterpreter();
 
-		void setup(const Setting& setting);
-		void initialize(const char* programName);
-		void addModule(const char* name, PyMethodDef* methods);
-		void addModule(const char* name, PyMethodDef* methods, const Dictionary<String, int> intConstants, const Dictionary<String, String> stringConstants);
+        void setup(const Setting& setting);
+        void initialize(const char* programName);
+        void addModule(const char* name, PyMethodDef* methods);
+        void addModule(const char* name, PyMethodDef* methods, const Dictionary<String, int> intConstants, const Dictionary<String, String> stringConstants);
 
-		//! Immediately executes a script statement on the local node.
-		void eval(const String& script, const char* format = NULL, ...);
-		//! Execute a script file.
-		//! The script path accepts two macros:
-		//! OMEGA_DATA_ROOT will be substituted with the default data directory for the omegalib installation
-		//! OMEGA_APP_ROOT will be substituted with the application directory for the installation (set throguh CMake at build configuration time)
-		void runFile(const String& filename, uint flags = DefaultRunFlags);
-		//! Cleans the application state.
-		void clean();
-		//! Cleans the application state and runs a new script.
-		void cleanRun(const String& filename);
+        //! Immediately executes a script statement on the local node.
+        void eval(const String& script, const char* format = NULL, ...);
+        //! Execute a script file.
+        //! The script path accepts two macros:
+        //! OMEGA_DATA_ROOT will be substituted with the default data directory for the omegalib installation
+        //! OMEGA_APP_ROOT will be substituted with the application directory for the installation (set throguh CMake at build configuration time)
+        void runFile(const String& filename, uint flags = DefaultRunFlags);
+        //! Cleans the application state.
+        void clean();
+        //! Cleans the application state and runs a new script.
+        void cleanRun(const String& filename);
 
-		//! Executes an event command statement.
-		//! @remarks Event command statements are commonly used as event handlers. 
-		//! The event passed to this call can be accessed from the script side using the
-		//! getEvent() global function
-		void evalEventCommand(const String& command, const Event& evt);
+        //! Executes an event command statement.
+        //! @remarks Event command statements are commonly used as event handlers. 
+        //! The event passed to this call can be accessed from the script side using the
+        //! getEvent() global function
+        void evalEventCommand(const String& command, const Event& evt);
 
-		//! Queues a command for execution. If the local flag is set, the command will be executed only on
-		//! the local node.
-		void queueCommand(const String& command, bool local = false);
+        //! Queues a command for execution. If the local flag is set, the command will be executed only on
+        //! the local node.
+        void queueCommand(const String& command, bool local = false);
 
-		void registerCallback(void* callback, CallbackType type);
-		void unregisterAllCallbacks();
+        void registerCallback(void* callback, CallbackType type);
+        void unregisterAllCallbacks();
 
-		void addPythonPath(const char*);
+        void addPythonPath(const char*);
 
-		bool isEnabled();
-		bool isShellEnabled() { return myShellEnabled; }
+        bool isEnabled();
+        bool isShellEnabled() { return myShellEnabled; }
 
-		// invoke python callbacks.
-		void update(const UpdateContext& context);
-		void handleEvent(const Event& evt);
-		void draw(const DrawContext& context, Camera* cam);
+        // invoke python callbacks.
+        void update(const UpdateContext& context);
+        void handleEvent(const Event& evt);
+        void draw(const DrawContext& context, Camera* cam);
 
-		// Shared data
-		virtual void commitSharedData(SharedOStream& out);
-		virtual void updateSharedData(SharedIStream& in);
+        // Shared data
+        virtual void commitSharedData(SharedOStream& out);
+        virtual void updateSharedData(SharedIStream& in);
 
-		//String getHelpString(const String& filter);
+        //String getHelpString(const String& filter);
         void lockInterpreter();
         void unlockInterpreter();
 
     protected:
-		struct QueuedCommand
-		{
-			QueuedCommand(const String& cmd, bool bneedsExecute, bool bneedsSend):
-				command(cmd), needsExecute(bneedsExecute), needsSend(bneedsSend)
-			{}
-			String command;
-			bool needsExecute;
-			bool needsSend;
-		};
+        struct QueuedCommand
+        {
+            QueuedCommand(const String& cmd, bool bneedsExecute, bool bneedsSend):
+                command(cmd), needsExecute(bneedsExecute), needsSend(bneedsSend)
+            {}
+            String command;
+            bool needsExecute;
+            bool needsSend;
+        };
 
-	protected:
-		bool myEnabled;
-		bool myShellEnabled;
-		bool myDebugShell;
-		// Command read from a configuration file and executed during 
-		// initialization. Helpful to load or setup optional modules.
-		String myInitCommand;
+    protected:
+        bool myEnabled;
+        bool myShellEnabled;
+        bool myDebugShell;
+        // Command read from a configuration file and executed during 
+        // initialization. Helpful to load or setup optional modules.
+        String myInitCommand;
         String myInitScript;
 
         PythonInteractiveThread* myInteractiveThread;
 
-		Lock myInteractiveCommandLock;
-		List<QueuedCommand*> myCommandQueue;
+        Lock myInteractiveCommandLock;
+        List<QueuedCommand*> myCommandQueue;
 
-		List<void*> myUpdateCallbacks;
-		List<void*> myEventCallbacks;
-		List<void*> myDrawCallbacks;
+        List<void*> myUpdateCallbacks;
+        List<void*> myEventCallbacks;
+        List<void*> myDrawCallbacks;
 
-		//char* myExecutablePath;
+        //char* myExecutablePath;
 
-		//List<CommandHelpEntry*> myHelpData;
+        //List<CommandHelpEntry*> myHelpData;
 
-		Lock myLock;
-		
-		// Stats
-		Ref<Stat> myUpdateTimeStat;
+        Lock myLock;
+        
+        // Stats
+        Ref<Stat> myUpdateTimeStat;
 
 
-	private:
-		static const Event* mysLastEvent;
-	};
+    private:
+        static const Event* mysLastEvent;
+    };
 
-	///////////////////////////////////////////////////////////////////////////
-	//! Implements a renderer command that runs a python statement when executed.
-	//class ScriptRendererCommand: public IRendererCommand
-	//{
-	//public:
-	//	ScriptRendererCommand()
-	//	{
-	//		myInterp = SystemManager::instance()->getScriptInterpreter();
-	//		myStatement = "";
-	//	}
+    ///////////////////////////////////////////////////////////////////////////
+    //! Implements a renderer command that runs a python statement when executed.
+    //class ScriptRendererCommand: public IRendererCommand
+    //{
+    //public:
+    //	ScriptRendererCommand()
+    //	{
+    //		myInterp = SystemManager::instance()->getScriptInterpreter();
+    //		myStatement = "";
+    //	}
 
-	//	void setStatement(const String& value) { myStatement = value; }
+    //	void setStatement(const String& value) { myStatement = value; }
 
-	//	void execute(Renderer* r)
-	//	{
-	//		myInterp->eval(myStatement);
-	//	}
+    //	void execute(Renderer* r)
+    //	{
+    //		myInterp->eval(myStatement);
+    //	}
 
-	//private:
-	//	String myStatement;
-	//	PythonInterpreter* myInterp;
-	//};
+    //private:
+    //	String myStatement;
+    //	PythonInterpreter* myInterp;
+    //};
 };
 #endif
 
