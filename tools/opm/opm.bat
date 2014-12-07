9d643d72aa89ee0214d4efeb616bdaa8d51a3dea
Alessandro Febretti <febret@gmail.com>
2014-12-06 23:50:16 -0600

    NEW: opm

---
+++ tools/opm/opm.bat
@@ -0,0 +1,1 @@
+@cmake -DARG1="%1" -DARG2="%2" -DARG3="%3" -DARG4="%4" -P opm.cmake
---
+++ tools/opm/opm.cmake
@@ -0,0 +1,4 @@
+# Download an update to this and the rest of the opm scripts.
+set(OPM_URL "https://raw.githubusercontent.com/uic-evl/omegalib/master/tools/opm")
+file(DOWNLOAD ${OPM_URL}/opm.cmake opm.cmake)
+file(DOWNLOAD ${OPM_URL}/info.cmake opm/info.cmake)
