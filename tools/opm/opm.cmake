2bf0b794b5a1a1a89b1c0f5fd67a88bbe8c468d5
Alessandro Febretti <febret@gmail.com>
2014-12-06 23:59:32 -0600

    Update opm.cmake

--- tools/opm/opm.cmake
+++ tools/opm/opm.cmake
@@ -1,4 +1,6 @@
 # Download an update to this and the rest of the opm scripts.
 set(OPM_URL "https://raw.githubusercontent.com/uic-evl/omegalib/master/tools/opm")
-file(DOWNLOAD ${OPM_URL}/opm.cmake ./opm.cmake SHOW_PROGRESS )
-file(DOWNLOAD ${OPM_URL}/info.cmake ./opm/info.cmake SHOW_PROGRESS )
+message("updating opm...")
+file(DOWNLOAD ${OPM_URL}/opm.cmake ./opm.cmake)
+file(DOWNLOAD ${OPM_URL}/info.cmake ./opm/info.cmake)
+message("done.")
