3c2f62224152a896174ffd7644ce5eaa01c4124d
Alessandro Febretti <febret@gmail.com>
2015-01-20 22:35:03 -0600

    FIX: Navigation+picking broken #133

--- src/omegaToolkit/DefaultMouseInteractor.cpp
+++ src/omegaToolkit/DefaultMouseInteractor.cpp
@@ -1,44 +1,48 @@
-/**************************************************************************************************
- * THE OMEGA LIB PROJECT
- *-------------------------------------------------------------------------------------------------
- * Copyright 2010-2015		Electronic Visualization Laboratory, University of Illinois at Chicago
- * Authors:										
- *  Alessandro Febretti		febret@gmail.com
- *-------------------------------------------------------------------------------------------------
- * Copyright (c) 2010-2015, Electronic Visualization Laboratory, University of Illinois at Chicago
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions 
- * and the following disclaimer. Redistributions in binary form must reproduce the above copyright 
- * notice, this list of conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF 
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *************************************************************************************************/
+/******************************************************************************
+* THE OMEGA LIB PROJECT
+*-----------------------------------------------------------------------------
+* Copyright 2010-2015		Electronic Visualization Laboratory,
+*							University of Illinois at Chicago
+* Authors:
+*  Alessandro Febretti		febret@gmail.com
+*-----------------------------------------------------------------------------
+* Copyright (c) 2010-2015, Electronic Visualization Laboratory,
+* University of Illinois at Chicago
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer. Redistributions in binary
+* form must reproduce the above copyright notice, this list of conditions and
+* the following disclaimer in the documentation and/or other materials provided
+* with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*-----------------------------------------------------------------------------
+* What's in this file
+* An actor to move/rotate scene nodes using a mouse.
+******************************************************************************/
 #include "omegaToolkit/DefaultMouseInteractor.h"
 #include "omega/DisplaySystem.h"
 
 using namespace omegaToolkit;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void DefaultMouseInteractor::handleEvent(const Event& evt)
 {
 	if((evt.getServiceType() == Service::Pointer 
 	|| evt.getServiceType() == Service::Wand) && !evt.isProcessed())
 	{
-		// If a node is assigned to this actor and is selected, we consider mouse events consumed
-		// by this actor.
-		if(myNode != NULL && myNodeActive) evt.setProcessed();
-
 		myPointerButton1Pressed = false;
 		myPointerButton2Pressed = false;
 		myPointerEventReceived = true;
@@ -62,10 +66,14 @@ void DefaultMouseInteractor::handleEvent(const Event& evt)
 		if(evt.getType() == Event::Zoom) myPointerEventData = evt.getExtraDataInt(0);
 
 		updateNode();
-	}
+
+        // If a node is assigned to this actor and is selected, we consider mouse events consumed
+        // by this actor.
+        if(myNode != NULL && myNodeActive) evt.setProcessed();
+    }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void DefaultMouseInteractor::updateNode()
 {
 	// Exit immediately if we received no pointer event or if there is no node attached to this
