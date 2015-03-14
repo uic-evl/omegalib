e9bd228c9f78e59d091b55a115dcf509a640d01b
Alessandro Febretti <febret@gmail.com>
2015-03-14 14:20:23 -0500

    updated maintenance utils

--- tools/maintenance-utils/omega.cmake
+++ tools/maintenance-utils/omega.cmake
@@ -3,7 +3,7 @@ set(OPM_URL "http://omegalib.s3.amazonaws.com/maintenance-utils")
 
 find_package(Git REQUIRED QUIET)
 
-set(SCRIPTS "omega;get;build;add;update")
+set(SCRIPTS "omega;get;build;add;update;pack;pack-build")
 foreach(S IN LISTS SCRIPTS)
     file(DOWNLOAD ${OPM_URL}/${S}.cmake ./cmake/${S}.cmake)
 endforeach()
@@ -11,7 +11,7 @@ endforeach()
 # invoke a script
 if("${ARG1}" STREQUAL "")
     message("Welcome to the omegalib maintenance utility")
-    message("SYNTAX: omega [get|build|add|update]")
+    message("SYNTAX: omega [get|build|add|update|pack|pack-build]")
     message("  Type omega followed by one of the supported commands to get help")
     message("  for that command.")
     message("  If you just want to quickly install omegalib type:")
