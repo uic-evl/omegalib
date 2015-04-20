2405dedc2a3ccee21fb09808ba3f48ead4be029d
Connor Taffe <cpaynetaffe@gmail.com>
2015-04-20 15:59:16 -0500

    fixed example enum in glLightfv call

--- examples/ohello2/ohello2.cpp
+++ examples/ohello2/ohello2.cpp
@@ -1,30 +1,30 @@
-/********************************************************************************************************************** 
+/**********************************************************************************************************************
  * THE OMEGA LIB PROJECT
  *---------------------------------------------------------------------------------------------------------------------
  * Copyright 2010								Electronic Visualization Laboratory, University of Illinois at Chicago
- * Authors:										
+ * Authors:
  *  Alessandro Febretti							febret@gmail.com
  *---------------------------------------------------------------------------------------------------------------------
  * Copyright (c) 2010, Electronic Visualization Laboratory, University of Illinois at Chicago
  * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions and the following 
- * disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
- * and the following disclaimer in the documentation and/or other materials provided with the distribution. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
- * INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR 
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE 
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+ * disclaimer. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+ * and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
  * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *---------------------------------------------------------------------------------------------------------------------
  *	ohello2
- *		A slightly more complex version of ohello, implementing event handling and data sharing. ohello2 defines a 
- *		new class, HelloServer, that reads mouse events and uses it to rotate the rendered object. 
+ *		A slightly more complex version of ohello, implementing event handling and data sharing. ohello2 defines a
+ *		new class, HelloServer, that reads mouse events and uses it to rotate the rendered object.
  *		HelloServer also implements SharedObject to synchronize rotation data across all nodes.
  *********************************************************************************************************************/
 #include <omega.h>
@@ -46,7 +46,7 @@ private:
 	HelloApplication* myApplication;
 
 	Vector3s myNormals[6];
-	Vector4i myFaces[6]; 
+	Vector4i myFaces[6];
 	Vector3s myVertices[8];
 	Color myFaceColors[6];
 };
@@ -57,8 +57,8 @@ class HelloApplication: public EngineModule
 public:
 	HelloApplication(): EngineModule("HelloApplication") { enableSharedData(); }
 
-	virtual void initializeRenderer(Renderer* r) 
-	{ 
+	virtual void initializeRenderer(Renderer* r)
+	{
 		r->addRenderPass(new HelloRenderPass(r, this));
 	}
 
@@ -123,21 +123,21 @@ void HelloRenderPass::render(Renderer* client, const DrawContext& context)
 		// Enable depth testing and lighting.
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_LIGHTING);
-	
+
 		// Setup light.
 		glEnable(GL_LIGHT0);
 		glEnable(GL_COLOR_MATERIAL);
-		glLightfv(GL_LIGHT0, GL_COLOR, Color(1.0, 1.0, 1.0).data());
+		glLightfv(GL_LIGHT0, GL_DIFFUSE, Color(1.0, 1.0, 1.0).data());
 		glLightfv(GL_LIGHT0, GL_POSITION, Vector3s(0.0f, 0.0f, 1.0f).data());
 
 		// Draw a rotating teapot.
-		glTranslatef(0, 2, -2); 
+		glTranslatef(0, 2, -2);
 		glRotatef(10, 1, 0, 0);
 		glRotatef(myApplication->getYaw(), 0, 1, 0);
 		glRotatef(myApplication->getPitch(), 1, 0, 0);
 
 		// Draw a box
-		for (int i = 0; i < 6; i++) 
+		for (int i = 0; i < 6; i++)
 		{
 			glBegin(GL_QUADS);
 			glColor3fv(myFaceColors[i].data());
@@ -159,7 +159,7 @@ void HelloApplication::handleEvent(const Event& evt)
 {
 	if(evt.getServiceType() == Service::Pointer)
 	{
-		// Normalize the mouse position using the total display resolution, 
+		// Normalize the mouse position using the total display resolution,
 		// then multiply to get 180 degree rotations
 		DisplaySystem* ds = getEngine()->getDisplaySystem();
 		Vector2i resolution = ds->getDisplayConfig().getCanvasRect().size();
