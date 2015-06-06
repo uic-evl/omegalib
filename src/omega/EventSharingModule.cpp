6195ec7954c5e9c4bb2f0c15934a6dbcea9dc895
Alessandro Febretti <febret@gmail.com>
2015-06-06 16:12:28 -0500

    updated comments

--- src/omega/EventSharingModule.cpp
+++ src/omega/EventSharingModule.cpp
@@ -1,29 +1,37 @@
-/**************************************************************************************************
- * THE OMEGA LIB PROJECT
- *-------------------------------------------------------------------------------------------------
- * Copyright 2010-2015		Electronic Visualization Laboratory, University of Illinois at Chicago
- * Authors:										
- *  Alessandro Febretti		febret@gmail.com
- *-------------------------------------------------------------------------------------------------
- * Copyright (c) 2010-2015, Electronic Visualization Laboratory, University of Illinois at Chicago
- * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
- * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions 
- * and the following disclaimer. Redistributions in binary form must reproduce the above copyright 
- * notice, this list of conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. 
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF 
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *************************************************************************************************/
+/******************************************************************************
+* THE OMEGA LIB PROJECT
+*-----------------------------------------------------------------------------
+* Copyright 2010-2015		Electronic Visualization Laboratory,
+*							University of Illinois at Chicago
+* Authors:
+*  Alessandro Febretti		febret@gmail.com
+*-----------------------------------------------------------------------------
+* Copyright (c) 2010-2015, Electronic Visualization Laboratory,
+* University of Illinois at Chicago
+* All rights reserved.
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*
+* Redistributions of source code must retain the above copyright notice, this
+* list of conditions and the following disclaimer. Redistributions in binary
+* form must reproduce the above copyright notice, this list of conditions and
+* the following disclaimer in the documentation and/or other materials provided
+* with the distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*-----------------------------------------------------------------------------
+* What's in this file
+*	A module that shares input events with slave nodes on a cluster system
+******************************************************************************/
 #include "omega/EventSharingModule.h"
 //#include "eqinternal/eqinternal.h"
 
@@ -86,7 +94,7 @@ namespace omicron {
     };
 };
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 EventSharingModule::EventSharingModule():
 	EngineModule("EventSharingModule"),
 	myQueuedEvents(0)
@@ -95,7 +103,7 @@ EventSharingModule::EventSharingModule():
 	enableSharedData();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EventSharingModule::clearQueue()
 {
 	mysInstance->myQueueLock.lock();
@@ -105,7 +113,7 @@ void EventSharingModule::clearQueue()
 
 bool sEventDropNotified = false;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EventSharingModule::share(const Event& evt)
 {
 	if(mysInstance != NULL)
@@ -141,7 +149,7 @@ void EventSharingModule::share(const Event& evt)
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EventSharingModule::commitSharedData(SharedOStream& out)
 {
 	myQueueLock.lock();
@@ -156,7 +164,7 @@ void EventSharingModule::commitSharedData(SharedOStream& out)
 	myQueueLock.unlock();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EventSharingModule::updateSharedData(SharedIStream& in)
 {
 	// Read the events from the network data stream, and send them to the engine for processing.
@@ -187,7 +195,7 @@ void EventSharingModule::updateSharedData(SharedIStream& in)
 	myQueueLock.unlock();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EventSharingModule::dispose()
 {
 	//omsg("EventSharingModule::dispose");
