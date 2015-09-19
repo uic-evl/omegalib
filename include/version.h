b4729f202e9e344671f926ea2b9cf03e3461282d
Alessandro Febretti <febret@gmail.com>
2015-09-18 23:45:41 -0500

    VERSION: 8.0.6

--- include/version.h
+++ include/version.h
@@ -3,7 +3,7 @@
 
 #define OMEGA_VERSION_MAJOR 8
 #define OMEGA_VERSION_MINOR 0
-#define OMEGA_VERSION_REVISION 5
+#define OMEGA_VERSION_REVISION 6
 
 #define _VSTH(v) #v
 #define _VERSTR(v) _VSTH(v)
