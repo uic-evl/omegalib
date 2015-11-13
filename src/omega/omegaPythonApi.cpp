75fc3f0db482ed34a38df86960bcce42a9db75e0
Alessandro Febretti <febret@gmail.com>
2015-11-13 16:01:10 -0600

    NEW: mission control log forwarding python API

--- src/omega/omegaPythonApi.cpp
+++ src/omega/omegaPythonApi.cpp
@@ -1624,6 +1624,8 @@ BOOST_PYTHON_MODULE(omega)
         PYAPI_METHOD(MissionControlClient, setClientConnectedCommand)
         PYAPI_METHOD(MissionControlClient, setClientDisconnectedCommand)
         PYAPI_METHOD(MissionControlClient, setClientListUpdatedCommand)
+        PYAPI_METHOD(MissionControlClient, isLogForwardingEnabled)
+        PYAPI_METHOD(MissionControlClient, setLogForwardingEnabled)
         ;
 
     // MissionControlClient
