565d9dd089f16d6592db374fb4a7159bf2bd25c6
Alessandro Febretti <febret@gmail.com>
2015-12-12 00:18:57 -0600

    updated mtools

--- tools/mtools/pack.app.cmake
+++ tools/mtools/pack.app.cmake
@@ -51,9 +51,9 @@ function(pack_module MODULE_NAME MODULE_DIR)
 endfunction()
 
 function(create_launcher APPLICATION_NAME APP_SCRIPT)
-    file(WRITE ${ARG2}/install/launcher_tmp.au3 "Run(\"bin\\orun.exe ${APP_SCRIPT}.py -D \" & @ScriptDir & \" \" & $CmdLineRaw, \"\")")
+    file(WRITE ${ARG2}/install/launcher_tmp.au3 "Run(\"bin\\orun.exe modules/${MODULE_NAME}/${APP_SCRIPT}.py -D \" & @ScriptDir & \" \" & $CmdLineRaw, \"\")")
     set(LAUNCHER_COMPILER ${ARG2}/tools/Aut2exe.exe)
-    set(ICON_FILE ${APP_DIR}/${ARG3}.ico)
+    set(ICON_FILE ${MODULE_DIR}/${APP_SCRIPT}.ico)
     if(NOT EXISTS ${ICON_FILE})
         set(ICON_FILE ${ARG2}/install/config/omega64-transparent.ico)
     endif()
@@ -66,6 +66,41 @@ endfunction()
 
 function(pack_disable NAME)
     set(PACK_${NAME} false CACHE INTERNAL "")
+endfunction()
+
+function(pack_native_module NAME)
+    if(WIN32)
+        file(INSTALL DESTINATION ${PACKAGE_DIR}/bin
+            TYPE FILE
+            FILES
+                ${BIN_DIR}/${NAME}.pyd
+            )
+    elseif(APPLE)
+        file(INSTALL DESTINATION ${PACKAGE_DIR}/bin
+            TYPE FILE
+            FILES
+                ${BIN_DIR}/${NAME}.so
+            )
+    endif()
+endfunction()
+
+#-------------------------------------------------------------------------------
+# For simple script modules, this macro will package the full module directory
+macro(pack_module_dir)
+    file(INSTALL DESTINATION ${PACKAGE_DIR}/modules
+        TYPE DIRECTORY
+        FILES
+            ${MODULE_DIR}
+        PATTERN ".git" EXCLUDE
+        )
+endmacro()
+
+function(pack_dir DIRECTORY)
+    file(INSTALL DESTINATION ${PACKAGE_DIR}/modules/${MODULE_NAME}
+        TYPE DIRECTORY
+        FILES
+            ${MODULE_DIR}/${DIRECTORY}
+        )
 endfunction()
 
 
