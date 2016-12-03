76b553799175b8f9ab4b5d979e778437de3c1f7c
Alessandro Febretti <febret@gmail.com>
2016-12-02 23:20:16 -0600

    CHANGE: choose script opens a new shell with the settings

--- etc/mtools/choose.cmake
+++ etc/mtools/choose.cmake
@@ -8,6 +8,7 @@ set(CHOOSE_FILE ${CMAKE_CURRENT_LIST_DIR}/../../../choose_${ARG2}.bat)
 file(REMOVE ${CHOOSE_FILE})
 file(APPEND ${CHOOSE_FILE} "set OMEGA_HOME=${CMAKE_CURRENT_LIST_DIR}/../../\n")
 file(APPEND ${CHOOSE_FILE} "set PATH=${CMAKE_CURRENT_LIST_DIR}/../../build/bin/release;%PATH%\n")
+file(APPEND ${CHOOSE_FILE} "cmd")
 
 message("Choose script prepared")
 message("call choose_${ARG2}.bat to switch your omegalib environment to ${ARG2}")
